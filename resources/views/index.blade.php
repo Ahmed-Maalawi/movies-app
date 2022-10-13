0fd9d9c97a762348bfe858b3c4d276a477e5fc55
Ahmed El-Maalawi <ahmedelmaalawi@gmail.com>
2022-10-13 16:41:46 +0200

    crate navbar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>index movies page</h1>
+@endsection
---
+++ resources/views/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>show movie details page</h1>
+@endsection
